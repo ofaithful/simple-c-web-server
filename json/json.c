c126e1280c0e5431131e6b664cbb718b9e3d99ed
Alexander <aleksandry11@gmail.com>
2023-06-15 19:05:08 +0300

    finish lexer

--- json/json.c
+++ json/json.c
@@ -6,13 +6,24 @@
 
 #include "json.h"
 
+struct SLexer {
+    const char* input;
+    size_t inputLength;
+    size_t position;
+    size_t readPosition;
+    char ch;
+};
+
 static void _lexerReadChar(Lexer* lexer);
 static char _lexerPeek(Lexer* lexer);
 static void _lexerSkipWhitespace(Lexer* lexer);
+
 static const char* _lexerReadString(Lexer* lexer, size_t* len);
 static const char* _lexerReadNumber(Lexer* lexer, size_t* len);
+static const char* _lexerReadIdentifier(Lexer* lexer, size_t* len);
 
 static uint8_t _isNumber(char ch);
+static uint8_t _isLetter(char ch);
 
 static TokenType _getTokenTypeFromLiteral(const char* literal, size_t len);
 
@@ -25,8 +36,7 @@ Lexer* lexerCreate(const char* input) {
     lexer->inputLength = strlen(input);
     lexer->position = 0;
     lexer->readPosition = 0;
-
-    _lexerReadChar(lexer);
+    lexer->ch = input[0];
 
     return lexer;
 }
@@ -47,37 +57,46 @@ Token* lexerNext(Lexer* lexer) {
             token = tokenCreate(TOKEN_LEFT_BRACKET, NULL);
             break;
         case ']':
-            token = createToken(TOKEN_RIGHT_BRACKET, NULL);
+            token = tokenCreate(TOKEN_RIGHT_BRACKET, NULL);
             break;
         case ',':
-            token = createToken(TOKEN_COMMA, NULL);
+            token = tokenCreate(TOKEN_COMMA, NULL);
             break;
         case ':':
-            token = createToken(TOKEN_COLON, NULL);
+            token = tokenCreate(TOKEN_COLON, NULL);
             break;
         case '\0':
             token = tokenCreate(TOKEN_EOF, NULL);
             break;
     }
 
+    // check for string
     if (lexer->ch == '\"') {
+        _lexerReadChar(lexer);
         size_t len = 0;
-        // char* string = NULL;
-        const char* string = _lexerReadString(lexer, &len); 
-
-        token = tokenCreate(TOKEN_STRING, strndup(string, len));
-        
+        const char* strp = _lexerReadString(lexer, &len); 
+        char str[len + 1];
+        strncpy(str, strp, len);
+        str[len] = '\0';
+        token = tokenCreate(TOKEN_STRING, strdup(str));
+       _lexerReadChar(lexer); 
         return token;
     } else if (_isNumber(lexer->ch)) {
         size_t len = 0;
-        char* literal = NULL;
         const char* ident = _lexerReadNumber(lexer, &len);
-
-        literal = strndup(ident, len);
-        token = tokenCreate(TOKEN_NUMBER, literal);
+        char number[len + 1];
+        strncpy(number, ident, len);
+        number[len] = '\0';
+        token = tokenCreate(TOKEN_NUMBER, strdup(number));
         return token;
+    } else if (_isLetter(lexer->ch)) {
+        size_t len = 0;
+        const char* identifier = _lexerReadIdentifier(lexer, &len);
+
+        TokenType type = _getTokenTypeFromLiteral(identifier, len);
+        return tokenCreate(type, NULL);
     }
-    
+
     if (!token) {
         token = tokenCreate(TOKEN_ILLEGAL, NULL);
     }
@@ -87,174 +106,175 @@ Token* lexerNext(Lexer* lexer) {
     return token;
 }
 
-typedef struct {
-    char* key;
-    struct JSON_value* value;
-} JSON_object;
+void lexerCleanup(Lexer** lexer) {
+    if (*lexer) {
+        free(*lexer);
+    }
 
-typedef struct {
-    struct JSON_value** elements;
-    int count;
-} JSON_array;
+    *lexer = NULL;
+}
 
-typedef struct JSON_value {
-    enum {
-        JSON_STRING,
-        JSON_NUMBER,
-        JSON_BOOL,
-        JSON_NULL,
-        JSON_OBJECT,
-        JSON_ARRAY
-    } type;
-    union {
-        char* string;
-        double number;
-        int boolean;
-        JSON_object object;
-        JSON_array array;
-    } data;
-} JSON_value;
+Token *tokenCreate(TokenType type, char* literal) {
+    size_t len = sizeof(Token);
+    Token* token = malloc(len);
+    memset(token, 0, len);
 
-int is_numeric(char *chr) {
-    int is_digit = isdigit(*chr);
-    int is_numeric = isdigit(*chr) || *chr == '.' || *chr == 'e' || *chr == 'E' || *chr == '+' || *chr == '-';
+    token->literal = literal;
+    token->type = type;
+
+    return token;
+}
+
+void tokenCleanup(Token** token) {
+    if (*token && (*token)->literal) {
+        free((*token)->literal);
+    }
+
+    if (*token) {
+        free(*token);
+    }
+
+    *token = NULL;
+}
+
+static void _lexerReadChar(Lexer* lexer) {
+    if (lexer->readPosition >= lexer->inputLength) {
+        lexer->ch = '\0';
+    } else {
+        lexer->ch = lexer->input[lexer->readPosition];
+    }
+
+    lexer->position = lexer->readPosition;
+    lexer->readPosition++;
+}
+
+static char _lexerPeek(Lexer* lexer) {
+    if (lexer->readPosition >= lexer->inputLength) {
+        return '\0';
+    } else {
+        return lexer->input[lexer->readPosition];
+    }
+}
+
+static void _lexerSkipWhitespace(Lexer* lexer) {
+    while (lexer->ch == ' ' || lexer->ch == '\t' || lexer->ch == '\n' || lexer->ch == '\r') {
+        _lexerReadChar(lexer);
+    }
+}
+
+static const char* _lexerReadString(Lexer* lexer, size_t* len) {
+    char* result = NULL;
+    size_t position = lexer->position;
+
+    while (lexer->ch && lexer->ch != '\"') {
+        _lexerReadChar(lexer);
+    }
+
+    if (len) {
+        *len = lexer->position - position;
+    }
+    return lexer->input + position;
+}
+
+static const char* _lexerReadNumber(Lexer* lexer, size_t* len) {
+    char* result = NULL;
+    size_t position = lexer->position;
+
+    while (_isNumber(lexer->ch)) {
+        _lexerReadChar(lexer);
+    }
+
+    if (len) {
+        *len = lexer->position - position;
+    }
+
+    return lexer->input + position;
+}
+
+static const char* _lexerReadIdentifier(Lexer* lexer, size_t* len) {
+    size_t position = lexer->position;
+
+    while (_isLetter(lexer->ch)) {
+        _lexerReadChar(lexer);
+    }
+
+    if (len) {
+        *len = lexer->position - position;
+    }
+
+    return lexer->input + position;
+}
+
+static TokenType _getTokenTypeFromLiteral(const char* literal, size_t len) {
+    if (strncmp(literal, "true", len) == 0) {
+        return TOKEN_JSON_TRUE;
+    } else if (strncmp(literal, "false", len) == 0) {
+        return TOKEN_JSON_FALSE;
+    } else if (strncmp(literal, "null", len) == 0) {
+        return TOKEN_JSON_NULL;
+    }
+}
+
+static uint8_t _isNumber(char ch) {
+    int is_digit = isdigit(ch);
+    int is_numeric = ch == '.' || ch == 'e' || ch == 'E' || ch == '+' || ch == '-';
     return is_digit || is_numeric;
 }
 
-void process_json_object(const char* object) {
+static uint8_t _isLetter(char ch) {
+    return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_';
+}
+
+// typedef struct {
+//     char* key;
+//     struct JSON_value* value;
+// } JSON_object;
+
+// typedef struct {
+//     struct JSON_value** elements;
+//     int count;
+// } JSON_array;
+
+// typedef struct JSON_value {
+//     enum {
+//         JSON_STRING,
+//         JSON_NUMBER,
+//         JSON_BOOL,
+//         JSON_NULL,
+//         JSON_OBJECT,
+//         JSON_ARRAY
+//     } type;
+//     union {
+//         char* string;
+//         double number;
+//         int boolean;
+//         JSON_object object;
+//         JSON_array array;
+//     } data;
+// } JSON_value;
+
+
+// void process_json_object(const char* object) {
     
-}
-
-void lex(const char* json, Token *array) {
-    char buffer[2000];
-    int i = 0;
-    while (*json != '\0') {
-        // skip leading whitespace
-        // printf("CURRENT CHAR: %c\n", *json);
-        while (*json && isspace(*json))
-            json++;
-        if (*json == '{') {
-            json++; 
-            array[i] = (Token){TOKEN_LEFT_BRACE, NULL};
-            i++;
-        }
-        if (*json == '}') {
-            json++; 
-            array[i] = (Token){TOKEN_RIGHT_BRACE, NULL};
-            i++;
-        }
-        if (*json == '[') {
-            json++; 
-            array[i] = (Token){TOKEN_LEFT_BRACKET, NULL};
-            i++;
-        }
-        if (*json == ']') {
-            json++; 
-            array[i] = (Token){TOKEN_RIGHT_BRACKET, NULL};
-            i++;
-        }
-        if (*json == ',') {
-            json++; 
-            array[i] = (Token){TOKEN_COMMA, NULL};
-            i++;
-        }
-        if (*json == ':') {
-            json++; 
-            array[i] = (Token){TOKEN_COLON, NULL};
-            i++;
-        }
-
-        // check string
-        if (*json == '\"') {
-            json++;
-
-            char *start = buffer;
-            while (*json && *json != '\"' && start < buffer + sizeof(buffer) - 1) {
-                if (*json == '\\') {
-                    json++;
-                }
-                *start++ = *json++;
-                // printf("char: %c ", *json);
-            }
-            *start = '\0';
-
-            if (*json != '\"') {
-                array[i] = (Token){TOKEN_UNDEFINED, NULL};
-                i++;
-            }
-            // printf("json state: %c\n", &json);
-            json++; // skip closing quote
-            array[i] = (Token){TOKEN_STRING, strdup(buffer)};
-            i++;
-            // printf("string: %s\n", buffer);
-        }
-        // printf("buffer p: %p\n", buffer);
-        // printf("json state: %c\n", *json);
-        // printf("buffer state: %s\n", buffer);
-        // check for number tokens
-        if (isdigit(*json) || *json == '-') {
-            char *start = buffer;
-            // int isNumeric = isdigit(*json) || *json == '.' || *json == 'e' || *json == 'E' || *json == '+' || *json == '-';
-            // printf("isNumeric: %d\n", isNumeric);
-            // printf("buffer p(isdigit check): %p\n", buffer);
-            // printf("is json true:%d\n", *json);
-            while (is_numeric((char *) json)) { 
-                // printf("is char[%c] numeric? %d\n", *json, is_numeric(json));
-                *start++ = *json++;
-                // printf("START:%s\n", start);
-            }
-            *start = '\0';
-            // printf("start string: %s\n", start);
-            //TODO: parse number
-            array[i] = (Token){TOKEN_NUMBER, strdup(buffer)};
-            i++;
-        }
-
-        while (*json && isspace(*json))
-            json++;
-
-        // check for boolean and null
-        // printf("current json: %s\n", json);
-        if (strncmp(json, "true", 4) == 0) {
-            // printf("true\n");
-            json += 4;
-            array[i] = (Token){TOKEN_JSON_TRUE, NULL};
-            i++;
-        }
-        if (strncmp(json, "false", 5) == 0) {
-            // printf("false\n");
-            json += 5;
-            array[i] = (Token){TOKEN_JSON_FALSE, NULL};
-            i++;
-        }
-        if (strncmp(json, "null", 4) == 0) {
-            // printf("null\n");
-            json += 4;
-            array[i] = (Token){TOKEN_JSON_NULL, NULL};
-            i++;
-        }
-
-        // array[i] = (Token){TOKEN_UNDEFINED, NULL};  
-        // i++;
-        // json++;
-    }
-}
-
-
+// }
 
 
 int main() {
-    const char *testString = "{\"protocol\":\"HTTP\",\"version\":1.1,\"methods\":[\"GET\",\"PUT\",\"POST\",\"PATCH\",\"DELETE\"],\"headers\":{\"content-type\":\"application/json\",\"content-length\":125,\"host\":\"localhost:8080\"},\"trueValue\":true,\"falseValue\":false}";
+    const char *testString = "{\"protocol\":\"HTTP\",\"version\":1.1,\"methods\":[\"GET\",\"PUT\",\"POST\",\"PATCH\",\"DELETE\"],\"headers\":{\"content-type\":\"application/json\",\"content-length\":125,\"host\":\"localhost:8080\"},\"nullValue\": null,\"trueValue\":true,\"falseValue\":false}";
     int length = strlen(testString);
 
-    printf("length of the string is: %d\n", length);
-    Token tokens[length];
+    Lexer* lexer = lexerCreate(testString);
+    Token* token = NULL;
 
-    lex(testString, tokens);
-
-
-    for (int i = 0; i < length; i++) {
-        printf("TOKEN: %s(%d), value: %s\n", getTokenName(tokens[i].type), tokens[i].type, tokens[i].value);
+    int i = 0;
+    while ((token = lexerNext(lexer)) && token->type != TOKEN_EOF) {
+        printf("Token[%d].type: %d.\nToken[%d].literal: %s\n\n", i, token->type, i, token->literal);
+        // printf("position: %d\n", lexer->position);
+        tokenCleanup(&token);
+        i++;
     }
+    // printf("from 207 position: %s\n", &testString[202]);
+
+    lexerCleanup(&lexer);
     return 0;
 }
